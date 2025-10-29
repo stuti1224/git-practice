9986677bccb1c339c9520b725655c1af6ecf139b
Stuti <stutipatel7667@gmail.com>
2025-10-29 16:45:15 -0400

    codescript.js

---
+++ script.js
@@ -0,0 +1,15 @@
+body {
+    background-color: #f0f0f0;
+    text-align: center;
+    font-family: Arial, sans-serif;
+  }
+  button {
+    padding: 10px;
+    background: #007bff;
+    color: white;
+    border: none;
+    border-radius: 5px;
+  }
+    button:hover {
+        background: #0056b3;
+    }  
