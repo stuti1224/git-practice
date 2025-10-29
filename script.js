39825322d4ab1537a89466ca41c03d4cd0476dca
Stuti <stutipatel7667@gmail.com>
2025-10-29 16:49:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-body {
-    background-color: #f0f0f0;
-    text-align: center;
-    font-family: Arial, sans-serif;
+function showMessage() {
+    alert("Hello, GitHub!");
   }
-  button {
-    padding: 10px;
-    background: #007bff;
-    color: white;
-    border: none;
-    border-radius: 5px;
-  }
-    button:hover {
-        background: #0056b3;
-    }  
+  
